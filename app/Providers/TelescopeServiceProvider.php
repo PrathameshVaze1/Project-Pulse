32f999d5e7de7d68b1ee9b502e5f8a9a3d5a360f
Prathamesh <prathameshvaze@gmail.com>
2024-07-26 00:14:55 +0530

    WIP On learning Passkeys

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -57,7 +57,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     {
         Gate::define('viewTelescope', function ($user) {
             return in_array($user->email, [
-                'prathameshvaze@gmail.com'
+                'prathameshvaze@gmail.com',
             ]);
         });
     }
